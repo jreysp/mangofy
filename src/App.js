66c2e9f7514025cc3509f32b52897fc04c2038ba
Helen Quach <helenquach@Helens-MacBook-Pro.local>
2021-11-13 23:59:58 -0800

    Added link to homepage.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './routes/Home.js'
+import Login from './routes/Login.js'
 import logo from './logo.svg';
 import './App.css';
+
+function NavBar() {
+  // TODO: add a title that changes depending on the current route
+  return (
+      <header className="App-header">
+          <div className="App-header-left">
+              <a href="/">
+                  <img className="logo" src={logo} />
+              </a>
+          </div>
+          <div className="App-header-right">
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+          </div>
+      </header>
+  );
+}
 
 function App() {
   return (
    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
    </BrowserRouter>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
     return (
         <div>
             <h1>Homepage</h1>
+            <div><Link to="/login">Login</Link></div>
         </div>
     );
 }
