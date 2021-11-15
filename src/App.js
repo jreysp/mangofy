f8a3818ab09be33175aa3e3bc9141b87f7c332e8
Helen Quach <helenquach@Helens-MacBook-Pro.local>
2021-11-14 20:53:09 -0800

    Added logo.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import NavBar from './components/NavBar.js'
 import Home from './routes/Home.js'
 import Register from './routes/Register.js'
-import logo from './logo.svg';
+import logo from './mangofy_logo-03.png';
 import './App.css';
 
 function App() {
   return (
    <BrowserRouter>
       <NavBar />
+      <img src={logo} className="logo"/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
