d4cb42b911685818c517a34263ef3a67a13f896b
nazcor <93461111+nazcor@users.noreply.github.com>
2021-11-22 16:23:38 -0800

    Added the username and password input boxes and the login button

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../logo-white.png';
 //THIS IS THE PAGE FOR LOGINS
@@ -7,10 +8,60 @@ function Home() {
         <div className="home">
             <img src={logo} className="logo-home"/>
             <h1>Welcome</h1>
-            <div>
-               {/* Code for login goes here */}
-               This is where the login form goes.
+            
+            {/*Code for inputting the Username*/}
+            <div className="Username">
+            <input 
+                id="username" 
+                placeholder="USERNAME"
+                type='text'
+                style={{
+                    paddingLeft: "15px",
+                    paddingRight: "70px",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                    fontFamily: "Mont Heavy",
+                    color: "orangered",
+                    marginBottom: "10px"
+                }}
+            />
             </div>
+
+            {/*Code for inputting the Password*/}
+            <div className="Password"></div>
+            <input 
+                id="password" 
+                placeholder="PASSWORD"
+                type='text'
+                style={{
+                    paddingLeft: "15px",
+                    paddingRight: "70px",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                    fontFamily: "Mont Heavy",
+                    color: "orangered",
+                    marginBottom: "20px"
+                }}  
+            />
+
+            <br></br>
+
+            {/*Code for Login button*/}
+            <button
+                style={{
+                    paddingLeft: "65px",
+                    paddingRight: "65px",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                    fontFamily: "Mont Heavy",
+                    color: "white",
+                    backgroundColor: "orange",
+                    marginBottom: "10px"
+                }} 
+            >
+                LOGIN
+            </button>
+
             <div><Link to="/register">Click Here to Register</Link></div>
         </div>
     );
