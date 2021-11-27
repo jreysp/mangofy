953b71293ac86541bc2f2e50669e9bb0b3a21aae
Helen Quach <helenquach@Helens-MacBook-Pro.local>
2021-11-26 23:35:57 -0800

    Search page now accepts input from input box

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,46 +1,32 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import parseSongs from '../backend/search.js'
 
 function Search() {
 
+    const searchStringRef = useRef()
     const [searchString, setSearchString] = useState("")
+
+    function handleOnChange() {
+        setSearchString(searchStringRef.current.value)
+    }
 
     return (
         <div className="search">
             <h1 className="center">Search</h1>
-            <SearchBar/>
-            <h4>
-                <div className="song">
-                    <li className="song_number">#</li>
-                    <li className="song_name">Title</li>
-                    <li className="song_artist">Artist</li>
-                    <li className="song_genre">Genre</li>
-                    <div><br/></div>
-                </div>
-            </h4>
-            <DisplaySongs input = {search("Pop")}/>
+            <input
+                id="searchbar"
+                onKeyUp={handleOnChange}
+                type="text"
+                name="search"
+                ref={searchStringRef}
+                placeholder="Search for songs"
+            />
+            <DisplaySongs input = {search(searchString)}/>
         </div>
     );
 }
 
-//if on click
-//<DisplaySongs input = {search(searched_string)}/>
-
-//Searching code for the search bar
-function SearchBar() {
-    //<DisplaySongs input = {search("Levitating")}/>
-    //onClick = {() => {console.log("Click");}}
-    return (
-      <input id="searchbar" onkeyup="DisplaySongs()" type="text"
-      name="search" placeholder="Search for songs"/>
-    );
-    /*<div>
-            <label for="site-search">Search for a song:<br></br></label>
-            <input type="search" id="site-search" name="q" aria-label="Search through site content"/>
-            <button>Search</button>
-        </div>*/ 
-  }
   function search(search_string)
   {
     const all_songs = new parseSongs();
@@ -54,22 +40,26 @@ function SearchBar() {
     //var all_songs = search(search_term);
   
     var array = [];
-    /*var html_obj = <h1>Song List:</h1>;
-    array.push(html_obj);*/
-    console.log(all_songs);
+    var html_obj = <h4>
+        <div className="song">
+            <li className="song_number">#</li>
+            <li className="song_name">Title</li>
+            <li className="song_artist">Artist</li>
+            <li className="song_genre">Genre</li>
+            <div><br/></div>
+        </div>
+    </h4>;
+
+    if (all_songs.input.length !== 0) {
+        array.push(html_obj);
+    }
+    //console.log(all_songs);
     //console.log(all_songs.input.length);
    for(var i = 0; i < all_songs.input.length; i++)
    {
     const name = all_songs.input[i].name;
     const artist = all_songs.input[i].artist;
     const genre = all_songs.input[i].genre;
-     
-    /*var temp = <div><p>
-      Song {i+1}:<br></br>
-      {name}<br></br>
-      Artist: {artist}<br></br>
-      Genre: {genre}<br></br>
-      </p></div>;*/
 
       var temp = <div className="song">
         <li className="song_number">{i+1}</li>
